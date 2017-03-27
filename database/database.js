41b29b75f9b165daffc2b1f9abe5d75e88fdc657
dratech <alfonsotech@nym.hush.com>
2017-03-27 16:23:05 -0700

    adds database schema and starter seed

---
+++ database/database.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const databaseName = 'tsphere'
+const connectionString = process.env.DATABASE_URL || `postgres://${process.env.USER}@localhost:5432/${databaseName}`
+const pgp = require('pg-promise')
+const database = pgp(connectionString)
+
+const getPastTweets = () => {
+  return database.any(`SELECT * FROM tweets ORDER BY content`)
+}
+
+const getTweetById = (id) => {
+  return database.one(`SELECT * FROM tweets WHERE id=$1`, [$1=id])
+}
+
+const getTweetByContent = (content) => {
+  return database.one(`SELECT * FROM tweets WHERE content=$1`, [$1='%' + conent = '%']) //how to build this query
+}
+
+const addTweet = (content, category) => {
+  database.any(`INSERT INTO  tweets (content, category) VALUES ($1, $2)`, [$1=content, $2=category])
+}
+
+const deleteDuplicates = () => {
+  database.any(`DELETE FROM tweets WHERE ctid NOT IN
+(SELECT max(ctid) FROM tweets GROUP BY tweets.*)`)
+}
+
+//TODO: function to select a tweet by id or content and alter its category
