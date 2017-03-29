a6346d299441dacfce48b17bd568b7700c4c7397
dratech <alfonsotech@nym.hush.com>
2017-03-28 17:25:31 -0700

    seeds database

--- database/dbschema.sql
+++ database/dbschema.sql
@@ -2,12 +2,6 @@ DROP TABLE IF EXISTS tweets;
 CREATE TABLE tweets
   (
     tweetid SERIAL PRIMARY KEY,
-    content VARCHAR(1718),
-    category VARCHAR(150)
+    category VARCHAR(150),
+    content VARCHAR(1718)
   );
-
--- seed
-INSERT INTO tweets (content, category) VALUES
-  ('This is a first tweet', 'ancient'),
-  ('This is a second tweet', 'modern'),
-  ('This is a third tweet', 'contemporary');
